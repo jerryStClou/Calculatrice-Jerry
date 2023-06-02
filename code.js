c5dc453aae91d7bd5bcdaf053615d58a02c99752
Jerry <jerry.clou@outlook.com>
2023-06-02 14:44:50 +0200

    3ème étape

--- code.js
+++ code.js
@@ -1,9 +1,21 @@
 nombre1 = document.querySelector("#nombre1");
 nombre2 = document.querySelector("#nombre2");
 egle = document.querySelector("#egale");
+reponse = document.querySelector(".reponse");
+boutonCalcul = document.querySelector("#boutonCalcul");
 
 egale.addEventListener("click",function(){
 console.log(nombre1.value);
 console.log(nombre2.value);
 
 });
+
+
+
+
+boutonCalcul.addEventListener("click",function(){
+    console.log(boutonCalcul.value);
+    
+    });
+    
+
