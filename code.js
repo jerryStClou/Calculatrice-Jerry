180a331567f5e4b78ab8d98ec49bed0faa0e58d0
Jerry <jerry.clou@outlook.com>
2023-06-02 15:27:07 +0200

    4ème étape fin de la création de la calculatrice

--- code.js
+++ code.js
@@ -1,21 +1,42 @@
 nombre1 = document.querySelector("#nombre1");
 nombre2 = document.querySelector("#nombre2");
-egle = document.querySelector("#egale");
+egale = document.querySelector("#egale");
 reponse = document.querySelector(".reponse");
 boutonCalcul = document.querySelector("#boutonCalcul");
+let a;
+let nbr1;
+let nbr2;
 
 egale.addEventListener("click",function(){
-console.log(nombre1.value);
-console.log(nombre2.value);
+nbr1 = Number(nombre1.value);
+nbr2 = Number(nombre2.value);
 
+a = boutonCalcul.value;
+switch (a) {
+    case '+':
+      console.log(nbr1+nbr2);
+      reponse.innerHTML = `<p>${nbr1+nbr2}</p>`;
+      break;
+    case '-':
+          console.log(nbr1-nbr2);
+          reponse.innerHTML = `<p>${nbr1-nbr2}</p>`;
+             break;
+     case '/':
+          console.log(nbr1/nbr2);
+          reponse.innerHTML = `<p>${nbr1/nbr2}</p>`;
+                break;
+    case '*':
+           console.log(nbr1*nbr2);
+           reponse.innerHTML = `<p>${nbr1*nbr2}</p>`;
+                 break;
+
+    default:
+       console.log("il y'a une erreur");
+   }
 });
 
 
 
 
-boutonCalcul.addEventListener("click",function(){
-    console.log(boutonCalcul.value);
-    
-    });
     
 
