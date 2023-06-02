f19157b3e1f94ebafc9158c9e443c271408af43a
Jerry <jerry.clou@outlook.com>
2023-06-02 14:36:57 +0200

    2ème étape récupération des élements et test

---
+++ code.js
@@ -0,0 +1,9 @@
+nombre1 = document.querySelector("#nombre1");
+nombre2 = document.querySelector("#nombre2");
+egle = document.querySelector("#egale");
+
+egale.addEventListener("click",function(){
+console.log(nombre1.value);
+console.log(nombre2.value);
+
+});
